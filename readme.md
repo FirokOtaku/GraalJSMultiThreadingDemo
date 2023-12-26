2dc7308387a1e0a824e55b85d21a0122d07556c7
FirokOtaku <351768593@outlook.com>
2023-12-26 17:29:41 +0800

    init commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# GraalJS Thread Pool Demo
+
+This repo contains demo code of multi-threading in GraalJS.
+
+And current answer is **NO, you can't**.
+
+**You can't use thread pool in GraalJS context**.
+
+JavaScript is designed to be single-threaded, and GraalJS just follows the design.
---
+++ src/main/java/firok/demo/graaljsthreadpool/Main.java
@@ -0,0 +1,52 @@
+package firok.demo.graaljsthreadpool;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.intellij.lang.annotations.Language;
+
+import java.util.concurrent.Executors;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        @SuppressWarnings("JSUnresolvedReference") @Language("JS")
+        var scriptContent = """
+                    const Executors = Java.type('java.util.concurrent.Executors')
+                    const Runnable = Java.extend(Java.type('java.lang.Runnable'))
+                    const TimeUnit = Java.type('java.util.concurrent.TimeUnit')
+                    const Thread = Java.type('java.lang.Thread')
+                    const AtomicInteger = Java.type('java.util.concurrent.atomic.AtomicInteger')
+                    const { log } = console
+                    
+                    const counter = new AtomicInteger(0)
+                    const pool = Executors.newFixedThreadPool(6)
+                    for(let step = 0; step < 10; step++)
+                    {
+                        log('commit child thread', step)
+                        
+                        pool.execute(new Runnable(function () {
+                            const timeWait = parseInt('' + Math.random() * 4) + 1
+                            log('child thread start', step, timeWait)
+                            Thread.sleep(timeWait * 1000)
+                            log('child thread end', step, counter.incrementAndGet())
+                        }))
+                    }
+                    pool.shutdown()
+                    pool.awaitTermination(1, TimeUnit.MINUTES)
+                    pool.close()
+                    
+                    log('main thread end', pool, counter.get())
+                    """;
+        try(var context = Context.newBuilder("js").allowCreateThread(true).allowAllAccess(true).build())
+        {
+            var source = Source.newBuilder("js", scriptContent, "main.js").build();
+            context.eval(source);
+        }
+        catch (Exception any)
+        {
+            System.err.println("failed to execute script");
+            any.printStackTrace(System.err);
+        }
+    }
+}
